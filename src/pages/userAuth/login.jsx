3a312002819fc09febc16540b81424ddde0e6303
MSK4477 <ks7997067@gmail.com>
2023-11-20 13:03:45 +0530

    completed

--- src/pages/userAuth/login.jsx
+++ src/pages/userAuth/login.jsx
@@ -37,7 +37,7 @@ const Login = () => {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center h-screen bg-gray-100">
+    <div className="flex flex-col justify-center items-center h-screen bg-white">
       {loader ? (
         <Loader />
       ) : (
